cd125396d0a05d8961e2ba39ee0afea1ad973e4c
xuxing258 <15116406518@163.com>
2025-12-02 13:07:06 +0800

    25051202

---
+++ book/assets/01-python基础.html-DYoovnj1.js
@@ -0,0 +1,76 @@
+import{_ as s,c as a,a as p,o as e}from"./app-BkadFxXL.js";const l={};function t(i,n){return e(),a("div",null,[...n[0]||(n[0]=[p(`<h2 id="基本工具详解" tabindex="-1"><a class="header-anchor" href="#基本工具详解"><span>基本工具详解</span></a></h2><div class="language-python line-numbers-mode" data-highlighter="prismjs" data-ext="py"><pre><code><span class="line"><span class="token number">1.</span>分辨电脑位数<span class="token punctuation">:</span> 一般市面上<span class="token number">64</span><span class="token operator">/</span><span class="token number">32</span> <span class="token punctuation">(</span>主流都是<span class="token number">64</span><span class="token punctuation">)</span> </span>
+<span class="line">    此电脑 <span class="token operator">-</span><span class="token operator">-</span> 右击 <span class="token operator">-</span><span class="token operator">-</span> 属性 系统类型</span>
+<span class="line">    win11 <span class="token operator">-</span><span class="token operator">-</span> 直接搜索系统</span>
+<span class="line">    </span>
+<span class="line"><span class="token number">2</span><span class="token punctuation">.</span>python<span class="token operator">/</span>pycharm编辑器下载</span>
+<span class="line">	<span class="token number">1.</span>官网</span>
+<span class="line">    	python官网：https<span class="token punctuation">:</span><span class="token operator">//</span>www<span class="token punctuation">.</span>python<span class="token punctuation">.</span>org<span class="token operator">/</span></span>
+<span class="line">        pycharm官网：pycharm：https<span class="token punctuation">:</span><span class="token operator">//</span>www<span class="token punctuation">.</span>jetbrains<span class="token punctuation">.</span>com<span class="token punctuation">.</span>cn<span class="token operator">/</span>en<span class="token operator">-</span>us<span class="token operator">/</span>pycharm<span class="token operator">/</span></span>
+<span class="line">    <span class="token number">2.</span>直接去我的百度网盘下载</span>
+<span class="line">    	安装工具<span class="token punctuation">:</span></span>
+<span class="line">		<span class="token punctuation">(</span><span class="token number">3.13</span><span class="token punctuation">)</span>班级百度网盘<span class="token punctuation">:</span></span>
+<span class="line">		链接<span class="token punctuation">:</span>https<span class="token punctuation">:</span><span class="token operator">//</span>pan<span class="token punctuation">.</span>baidu<span class="token punctuation">.</span>com<span class="token operator">/</span>s<span class="token operator">/</span>1ReLJvwgGWhOX4grq_zJZCg?pwd<span class="token operator">=</span><span class="token number">0313</span></span>
+<span class="line">		提取码<span class="token punctuation">:</span> <span class="token number">0313</span></span>
+<span class="line">        注意：此百度网盘链接也是以后我们上课上传资料的链接（以后上课的课件<span class="token operator">+</span>课后代码）我都会传入这个链接里面，所以同学自己保存好链接！</span>
+<span class="line">            </span>
+<span class="line">    <span class="token number">3</span><span class="token punctuation">.</span>python的本质还是一门 语言不是用来打开的我们装的是python环境是用来写<span class="token operator">+</span>运行python代码程序的 不是用来<span class="token string">&#39;打开的&#39;</span>python装好后 不会像其他程序一样 桌面出现图标! </span>
+<span class="line">    </span>
+<span class="line">    <span class="token number">4.</span>检查环境是否安装成功</span>
+<span class="line">        windows系统：查看安装路径： cmd 输入 where python  回车键 获取到Python位置</span>
+<span class="line">        快捷键<span class="token operator">/</span>win <span class="token operator">+</span> r 输入命令cmd <span class="token operator">-</span><span class="token operator">-</span><span class="token operator">&gt;</span>python<span class="token operator">-</span><span class="token operator">-</span><span class="token operator">&gt;</span>出现<span class="token operator">&gt;&gt;</span><span class="token operator">&gt;</span> 代表安装成功</span>
+<span class="line">        mac系统：直接搜索终端 输入python3命令即可</span>
+<span class="line">        查看安装路径：在终端中输入which python或which python3命令</span>
+<span class="line">    </span>
+<span class="line">    <span class="token number">5.</span>安装好python后出现的几个文件</span>
+<span class="line">    	idle<span class="token punctuation">:</span> Python自带简单的编辑软件</span>
+<span class="line">        Python3<span class="token punctuation">.</span><span class="token number">8</span><span class="token punctuation">:</span> 命令行交互式程序</span>
+<span class="line">        manuals<span class="token punctuation">:</span> 官方技术文档</span>
+<span class="line">        module doce<span class="token punctuation">:</span> 已经安装模块的文档</span>
+<span class="line">            </span>
+<span class="line">    <span class="token number">6</span><span class="token punctuation">.</span>pycharm 属于python主流的编辑软件</span>
+<span class="line">    	尽量统一使用<span class="token number">3.8</span><span class="token number">.10</span>版本以上的python <span class="token operator">/</span> <span class="token number">2023</span>专业版pycharm</span>
+<span class="line">        </span>
+<span class="line">    <span class="token number">7.</span>环境变量<span class="token punctuation">:</span></span>
+<span class="line">        作用是让电脑认识python能够识别他 如果没有配置 你在cmd里找不到python 相当于电脑不认识python</span>
+<span class="line">        手动添加系统环境变量：电脑搜索栏搜索编辑系统环境变量——》环境变量——》用户变量path中——》添加python安装路径以及Scripts文件夹路径</span>
+<span class="line">        </span>
+<span class="line">    <span class="token number">8.</span>手机学习<span class="token punctuation">:</span></span>
+<span class="line">        安卓手机：应用商店搜索python编译器IDE</span>
+<span class="line">    	苹果手机：应用商店搜索python3IDE</span>
+<span class="line">        百度网盘</span>
+<span class="line">        </span>
+<span class="line">    <span class="token number">9</span><span class="token punctuation">.</span>python运行方式</span>
+<span class="line">    	<span class="token number">1.</span>交互模式<span class="token punctuation">:</span></span>
+<span class="line">            默认进入python<span class="token operator">/</span>idle 表示符号<span class="token operator">&gt;&gt;</span><span class="token operator">&gt;</span></span>
+<span class="line">            交互模式其实就是你问我答 代码输入错误不能修改</span>
+<span class="line">            优点<span class="token punctuation">:</span>简单 快捷</span>
+<span class="line">            缺点<span class="token punctuation">:</span>不能修改 不好保存</span>
+<span class="line">            适合<span class="token punctuation">:</span>测试<span class="token operator">/</span>写简单的代码</span>
+<span class="line">                </span>
+<span class="line">        <span class="token number">2.</span>脚本模式<span class="token punctuation">(</span>文件模式<span class="token punctuation">)</span><span class="token punctuation">:</span>把python代码写在文件里运行 可以修改和撤回</span>
+<span class="line">            正式写代码都是用脚本模式</span>
+<span class="line">            优点<span class="token punctuation">:</span>可以修改 长期使用 <span class="token operator">-</span><span class="token operator">-</span> 基本都是用文件模式</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="输出" tabindex="-1"><a class="header-anchor" href="#输出"><span>输出</span></a></h2><div class="language-python line-numbers-mode" data-highlighter="prismjs" data-ext="py"><pre><code><span class="line">迈开学习第一步<span class="token punctuation">:</span>输出<span class="token operator">-</span><span class="token operator">-</span>第一个python程序<span class="token operator">-</span><span class="token operator">-</span>helloword</span>
+<span class="line">    输出<span class="token punctuation">:</span>说人话就是电脑屏幕 电视机 输出显示的内容</span>
+<span class="line">        </span>
+<span class="line">在pycharm里面输出字符串</span>
+<span class="line">	字符串就是多个字符的集合 由双引号<span class="token string">&quot;&quot;</span> 或者单引号 <span class="token string">&#39;&#39;</span> 进行包围数据</span>
+<span class="line">    字符串里可以包含所有数据</span>
+<span class="line">    </span>
+<span class="line"><span class="token keyword">print</span>输出字符串的格式如下<span class="token punctuation">:</span></span>
+<span class="line">    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&quot;字符串内容&quot;</span><span class="token punctuation">)</span></span>
+<span class="line">    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;字符串内容&#39;</span><span class="token punctuation">)</span></span>
+<span class="line">    <span class="token comment"># 在python中单引号/双引号没有区别</span></span>
+<span class="line">    </span>
+<span class="line">注意<span class="token punctuation">:</span>字符串要放在<span class="token punctuation">(</span><span class="token punctuation">)</span>中传递给<span class="token keyword">print</span> 让<span class="token keyword">print</span>把字符串显示到屏幕上 这种写法在python当中称为函数</span>
+<span class="line">    <span class="token number">1.</span>引号和小括号必须都是英文状态下输入 </span>
+<span class="line">    <span class="token number">2.</span><span class="token keyword">print</span>需要小写 Python里需注意单词大小写</span>
+<span class="line">    <span class="token number">3</span><span class="token punctuation">.</span>python国外发明的 所有的符号内容都是英文 <span class="token punctuation">(</span>shift键<span class="token operator">/</span>中英文切换<span class="token punctuation">)</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-python line-numbers-mode" data-highlighter="prismjs" data-ext="py"><pre><code><span class="line"><span class="token keyword">print</span><span class="token punctuation">(</span><span class="token number">1000</span><span class="token punctuation">)</span>  <span class="token comment"># 输出数字</span></span>
+<span class="line"><span class="token keyword">print</span><span class="token punctuation">(</span><span class="token number">1000</span> <span class="token operator">+</span> <span class="token number">2000</span><span class="token punctuation">)</span>  <span class="token comment"># 3000</span></span>
+<span class="line"><span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&quot;欧阳老师&quot;</span><span class="token punctuation">)</span>  <span class="token comment"># 输出文字</span></span>
+<span class="line"><span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&quot;+-*/,?&quot;</span><span class="token punctuation">)</span>  <span class="token comment"># 输出符号</span></span>
+<span class="line"><span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&quot;abcdefg&quot;</span><span class="token punctuation">)</span>  <span class="token comment"># 输出符号</span></span>
+<span class="line"><span class="token comment"># 整数和字符串不能加号运算</span></span>
+<span class="line"><span class="token comment"># print(&quot;10000&quot;+1000)</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5)])])}const o=s(l,[["render",t]]),r=JSON.parse('{"path":"/python/01-python%E5%9F%BA%E7%A1%80.html","title":"","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"基本工具详解","slug":"基本工具详解","link":"#基本工具详解","children":[]},{"level":2,"title":"输出","slug":"输出","link":"#输出","children":[]}],"git":{},"filePathRelative":"python/01-python基础.md"}');export{o as comp,r as data};
