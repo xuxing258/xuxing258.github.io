2e08b813756997e563c3bd2801751fbed3d6c170
xuxing258 <15116406518@163.com>
2026-08-01 13:33:26 +0800

    20260801

--- book/assets/01-python基础.html-C59TJ73r.js
+++
@@ -1,9 +0,0 @@
-import{_ as e,c as n,a as l,o as a}from"./app-MEd4_H3g.js";const i={};function t(c,s){return a(),n("div",null,[...s[0]||(s[0]=[l(`<h2 id="同局域网手机访问html-css-js文件" tabindex="-1"><a class="header-anchor" href="#同局域网手机访问html-css-js文件"><span>同局域网手机访问html css js文件</span></a></h2><p>通过python进行执行</p><ul><li><code>python -m http.server</code>：让 Python 运行其内置的 <code>http.server</code>模块。</li><li><code>8000</code>：指定服务器使用的端口号（你可以更改为 8080 等其他未被占用的端口）。</li></ul><div class="language-cmd line-numbers-mode" data-highlighter="prismjs" data-ext="cmd"><pre><code><span class="line">python -m http.server 8000</span>
-<span class="line"># 在对应的html位置通过终端进行执行</span>
-<span class="line"></span>
-<span class="line"># 按 Win + R，输入 cmd回车 </span>
-<span class="line"># 输入命令：ipconfig</span>
-<span class="line"># 找到 ​​IPv4 地址​​</span>
-<span class="line"></span>
-<span class="line">在手机的浏览器上就可以进行访问:  http://192.168.1.100:8000</span>
-<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4)])])}const r=e(i,[["render",t]]),p=JSON.parse('{"path":"/problem/01-python%E5%9F%BA%E7%A1%80.html","title":"","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"同局域网手机访问html css js文件","slug":"同局域网手机访问html-css-js文件","link":"#同局域网手机访问html-css-js文件","children":[]}],"git":{},"filePathRelative":"problem/01-python基础.md"}');export{r as comp,p as data};
